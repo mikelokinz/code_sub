1730fcf37b2e9a220cec65f957319d3357c91d06
Michael John Franklin <mikelokinz@gmail.com>
2026-07-22 08:30:02 +0530

    Add Sieve of Erato67henes [A] from Codeforces

---
+++ 2195/A - Sieve of Erato67henes/solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                int n = sc.nextInt();
+                boolean found = false;
+                for (int i = 0; i < n; i++) {
+                    if (sc.nextInt() == 67) {
+                        found = true;
+                    }
+                }
+                if (found) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+        sc.close();
+    }
+}
